094c91e2e73e211d45ca465cfa84c34fd85e77e7
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2024-02-24 12:23:58 +0000

    _

--- main.cpp
+++ main.cpp
@@ -21,11 +21,13 @@ int main()
 
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> colour_balloon(1, 3);
+    std::uniform_int_distribution<std::mt19937::result_type> colour_balloon(1, 4);
 
     std::random_device dev2;
     std::mt19937 rng2(dev2());
     std::uniform_int_distribution<std::mt19937::result_type> x_coord(1, 450);
+
+    bool should_add_another = true;
 
     while (window.isOpen())
     {
@@ -50,23 +52,39 @@ int main()
                             std::cout << score << "\n";
                         }
                     }
+                    should_add_another = true;
+                }
+                if (event.mouseButton.button == sf::Mouse::Right)
+                {
+                    std::cout << "new ballooon\n";
+                    int what_type = colour_balloon(rng);
+                    int what_x = x_coord(rng2);
+                    if (what_type == 2)
+                        balloons.push_back(Balloon{ sf::Color::Red , sf::Vector2f(what_x, 450) });
+                    if (what_type == 3)
+                        balloons.push_back(Balloon{ sf::Color::Green, sf::Vector2f(what_x, 450) });
+                    else
+                        balloons.push_back(Balloon{ sf::Color::Blue , sf::Vector2f(what_x, 450) });
+                    last = std::chrono::system_clock::now();
+                    std::cout << what_x << ", " << 450 << '\n';
                 }
             }
         }
 
-        if (std::chrono::system_clock::now() - last >= 0.25s)
+        if (should_add_another)
         {
             std::cout << "new ballooon\n";
             int what_type = colour_balloon(rng);
             int what_x = x_coord(rng2);
             if (what_type == 1)
-                balloons.push_back(Balloon{ sf::Color::Red , sf::Vector2f(what_x, 450)});
+                balloons.push_back(Balloon{ sf::Color::Red , sf::Vector2f(what_x, 450) });
             if (what_type == 2)
                 balloons.push_back(Balloon{ sf::Color::Green, sf::Vector2f(what_x, 450) });
             else
-                balloons.push_back(Balloon{ sf::Color::Blue , sf::Vector2f(what_x, 450)});
+                balloons.push_back(Balloon{ sf::Color::Blue , sf::Vector2f(what_x, 450) });
             last = std::chrono::system_clock::now();
             std::cout << what_x << ", " << 450 << '\n';
+            should_add_another = false;
         }
 
         if (std::chrono::system_clock::now() - last >= 0.0025s)
